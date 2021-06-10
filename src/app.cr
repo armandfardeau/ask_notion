e77b1ae565ad763467ae85bdc819925714d60c11
Armand Fardeau <fardeauarmand@gmail.com>
2021-06-10 17:58:43 +0200

    Log request

--- src/app.cr
+++ src/app.cr
@@ -8,7 +8,10 @@ server = HTTP::Server.new do |context|
   params = context.request.query_params
 
   context.response.content_type = "application/json"
-  context.response.print(params)
+  req = context.request.body.not_nil!.gets_to_end == "" ? "No request" : JSON.parse(context.request.body.not_nil!.gets_to_end)
+
+  puts req
+  context.response.print(req.to_json)
 end
 
 address = server.bind_tcp(HOST, PORT)
