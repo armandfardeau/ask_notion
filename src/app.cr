9a56539174281da7d3a616d076c8ab9a80ce54a1
Armand Fardeau <fardeauarmand@gmail.com>
2021-06-10 18:11:56 +0200

    Add todo

--- src/app.cr
+++ src/app.cr
@@ -5,7 +5,10 @@ PORT = ENV.has_key?("PORT") ? ENV["PORT"].to_i : 8080
 Kemal.config.port = PORT
 
 post "/" do
-  "Hello World!"
+  # TODO: Get question from rocketchat
+  # TODO: Check notion search for response
+  #     if empty, create a page and return the link
+  #     if not empty return the first 5 responses
 end
 
 Kemal.run
