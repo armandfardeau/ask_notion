05696bd82b496f182f56d4e593828bf0da8730c3
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-06-15 15:13:52 +0200

    catch parsing error when body empty

--- spec/app_spec.cr
+++ spec/app_spec.cr
@@ -1,8 +1,13 @@
 require "./spec_helper"
 
 describe "AskNotion" do
-  it "renders /" do
-    post "/"
-    response.status.should eq 401
+  context "when body is invalid" do
+    it "returns 500 status code" do
+      post("/", headers: HTTP::Headers{"Content-Type" => "application/json"}, body: "")
+
+      response.status_code.should eq 500
+      response.content_type.should eq "application/json"
+      response.body.should eq "Error when parsing body request, please ensure your body request is correct"
+    end
   end
 end
--- src/app.cr
+++ src/app.cr
@@ -21,41 +21,46 @@ module AskNotion
   end
 
   post "/" do |env|
-    # Get question from rocketchat
-    body = env.params.json
+    begin
+      # Get question from rocketchat
+      body = env.params.json
 
-    if check_rocket_token(body["token"], ROCKET_SECRET_TOKEN)
-      Log.info { "An unauthorized access has been recorded from #{env.request.remote_address} with #{body["token"]}" }
-      halt env, status_code: 401, response: "Unauthorized"
-    else
-      Log.info { "Tmid provided, doing nothing" } if body["tmid"]?
-      halt env, status_code: 200, response: "tmid provided, doing nothing" if body["tmid"]?
-
-      # Check notion search for response
-      room_id = body["channel_id"]
-      message_id = body["message_id"]
-      searched_text = body["text"]
-      request = search_in_notion(searched_text)
-
-      results = JSON.parse(request.body)["results"].as_a
-
-      if results.empty?
-        page_response = create_notion_page(searched_text)
-        page = JSON.parse(page_response.body)
-
-        Log.info { "Creating page: #{page}" }
-        response = send_to_rocket(room_id, message_id, page_message_builder(searched_text, page), CREATED_PAGE_MESSAGE)
-        halt env, status_code: 200, response: JSON.parse(response.body)
+      if check_rocket_token(body["token"], ROCKET_SECRET_TOKEN)
+        Log.info { "An unauthorized access has been recorded from #{env.request.remote_address} with #{body["token"]}" }
+        halt env, status_code: 401, response: "Unauthorized"
       else
-        responses = Array(Crest::Response).new
-        results.each do |result|
-          responses << send_to_rocket(room_id, message_id, search_message_builder(result))
-        end
+        Log.info { "Tmid provided, doing nothing" } if body["tmid"]?
+        halt env, status_code: 200, response: "tmid provided, doing nothing" if body["tmid"]?
 
-        returned_responses = responses.map { |response| JSON.parse(response.body) }.to_json
-        Log.info { "Returned_responses: #{returned_responses}" }
-        halt env, status_code: 200, response: returned_responses
+        # Check notion search for response
+        room_id = body["channel_id"]
+        message_id = body["message_id"]
+        searched_text = body["text"]
+        request = search_in_notion(searched_text)
+
+        results = JSON.parse(request.body)["results"].as_a
+
+        if results.empty?
+          page_response = create_notion_page(searched_text)
+          page = JSON.parse(page_response.body)
+
+          Log.info { "Creating page: #{page}" }
+          response = send_to_rocket(room_id, message_id, page_message_builder(searched_text, page), CREATED_PAGE_MESSAGE)
+          halt env, status_code: 200, response: JSON.parse(response.body)
+        else
+          responses = Array(Crest::Response).new
+          results.each do |result|
+            responses << send_to_rocket(room_id, message_id, search_message_builder(result))
+          end
+
+          returned_responses = responses.map { |response| JSON.parse(response.body) }.to_json
+          Log.info { "Returned_responses: #{returned_responses}" }
+          halt env, status_code: 200, response: returned_responses
+        end
       end
+    rescue ex : JSON::ParseException
+      Log.info { "Request from #{env.request.remote_address} - Body parsing error" }
+      halt env, status_code: 500, response: "Error when parsing body request, please ensure your body request is correct"
     end
   end
 
@@ -150,6 +155,7 @@ module AskNotion
 end
 
 Kemal.config.env = ENVIRONNEMENT
+serve_static false
 
 if Kemal.config.env == "production"
   Kemal.run do |config|
