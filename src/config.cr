eefc4bc057b84dd3723ca2890af3399aad49799a
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-06-18 13:25:55 +0200

    update config

--- src/config.cr
+++ src/config.cr
@@ -12,7 +12,7 @@ module AskNotion
     ROCKET_CHAT_URL      = ENV["ROCKET_CHAT_URL"]?.try(&.to_s) || "https://osp.rocket.chat"                                                            # Rocket chat server base url
     ROCKET_API_TOKEN     = ENV["ROCKET_API_TOKEN"]?.try(&.to_s) || ""                                                                                  # Rocket chat API token
     ROCKET_API_ID        = ENV["ROCKET_API_ID"]?.try(&.to_s) || ""                                                                                     # Rocket chat API ID
-    WIKI_PAGE_ID         = ENV["WIKI_PAGE_ID"]?.try(&.to_s) || "4df27551-820f-49ef-838d-cf2047110f47"                                                  # Notion Parent page ID, used for search
+    WIKI_PAGE_ID         = ENV["WIKI_PAGE_ID"]?.try(&.to_s) || "21705464-b054-4667-b49c-2749c14b2375"                                                  # Notion Parent page ID, used for search
     FAQ_PAGE_ID          = ENV["FAQ_PAGE_ID"]?.try(&.to_s) || "639e6e8d-73ab-45c8-a1b0-bf829d17c5e4"                                                   # Notion FAQ page ID, used for writing unknown questions
     CREATED_PAGE_MESSAGE = "Bonjour @here, une question a besoin de votre réponse. Si l'un-e d'entre vous a la réponse, n'hésitez pas à la compléter." # Default message sent on Rocket chat
     ENVIRONNEMENT        = ENV["KEMAL_ENV"]?.try(&.to_s) || "production"                                                                               # Kemal environnement, allows to run in production mode or running tests
