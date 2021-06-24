db61f73ba4f92516ddd29d6226a4dac6b179b491
Armand Fardeau <fardeauarmand@gmail.com>
2021-06-24 17:20:53 +0200

    Fix tests

--- spec/app_spec.cr
+++ spec/app_spec.cr
@@ -15,7 +15,7 @@ describe "AskNotion" do
     context "and token is invalid" do
       it "returns unauthorized response" do
         token = "dummy_token_unauthorized"
-        post("/", headers: HTTP::Headers{"Content-Type" => "application/json"}, body: rocket_chat_sample("dummy text", token).to_s)
+        post("/", headers: HTTP::Headers{"Content-Type" => "application/json"}, body: rocket_chat_sample("dummy text", token))
 
         response.content_type.should eq "application/json"
         response.status_code.should eq 401
@@ -27,7 +27,7 @@ describe "AskNotion" do
       it "returns 200 status code" do
         token = ""
         p AskNotion::Config::ROCKET_SECRET_TOKEN
-        post("/", headers: HTTP::Headers{"Content-Type" => "application/json"}, body: rocket_chat_sample("dummy text", token).to_s)
+        post("/", headers: HTTP::Headers{"Content-Type" => "application/json"}, body: rocket_chat_sample("dummy text", token))
 
         response.content_type.should eq "application/json"
         response.status_code.should eq 200
@@ -39,13 +39,9 @@ describe "AskNotion" do
       it "returns 200 status code" do
         web_mock_notion_search
         web_mock_notion_pages
+        web_mock_rocket_message
 
-        WebMock.stub(:post, "https://osp.rocket.chat/api/v1/chat.sendMessage")
-          .with(body: "{\"message\":{\"msg\":\"#{AskNotion::Config::CREATED_PAGE_MESSAGE}\",\"rid\":\"AzertYuiop4\",\"tmid\":\"dummy_message_id_1\",\"alias\":\"AskNotion\",\"avatar\":\"https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png\",\"attachments\":[{\"title\":\"dummy text\",\"title_link\":\"/dummy_id\",\"collapsed\":false}]}}", headers: {"Content-Type" => "application/json", "X-Auth-Token" => AskNotion::Config::ROCKET_SECRET_TOKEN, "X-User-Id" => AskNotion::Config::ROCKET_API_ID, "Content-Length" => "381", "Host" => "osp.rocket.chat", "User-Agent" => "Crest/0.27.0 (Crystal/1.0.0)"})
-          .to_return(body: {"results": ["dummy"] of String, "id": "dummy_id"}.to_json)
-
-        token = ""
-        post("/", headers: HTTP::Headers{"Content-Type" => "application/json"}, body: rocket_chat_sample("dummy text", token, tmid: nil).to_s)
+        post("/", headers: HTTP::Headers{"Content-Type" => "application/json"}, body: rocket_chat_sample("dummy text", "", tmid: nil))
 
         response.content_type.should eq "application/json"
         response.status_code.should eq 200
@@ -53,31 +49,32 @@ describe "AskNotion" do
         WebMock.reset
       end
     end
-    context "when request is unauthorized" do
-      it "returns no responses" do
-        web_mock_notion_search
-        web_mock_notion_pages
-        WebMock.stub(:post, "https://osp.rocket.chat/api/v1/chat.sendMessage")
-          .with(body: "{\"message\":{\"msg\":\"#{AskNotion::Config::CREATED_PAGE_MESSAGE}\",\"rid\":\"AzertYuiop4\",\"tmid\":\"dummy_message_id_1\",\"alias\":\"AskNotion\",\"avatar\":\"https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png\",\"attachments\":[{\"title\":\"dummy text\",\"title_link\":\"/dummy_id\",\"collapsed\":false}]}}", headers: {"Content-Type" => "application/json", "X-Auth-Token" => AskNotion::Config::ROCKET_SECRET_TOKEN, "X-User-Id" => AskNotion::Config::ROCKET_API_ID, "Content-Length" => "381", "Host" => "osp.rocket.chat", "User-Agent" => "Crest/0.27.0 (Crystal/1.0.0)"})
-          .to_return(status: 401)
-
-        response.content_type.should eq "application/json"
-        response.status_code.should eq 200
-        response.body.should eq "No response sent"
-        WebMock.reset
-      end
-    end
   end
 end
 
 def web_mock_notion_search
-  WebMock.stub(:post, "https://api.notion.com/v1/search")
-    .with(body: "{\"query\":\"dummy text\",\"page_size\":5,\"sort\":{\"direction\":\"ascending\",\"timestamp\":\"last_edited_time\"}}", headers: {"Content-Type" => "application/json", "Notion-Version" => AskNotion::Config::NOTION_API_VERSION, "Authorization" => AskNotion::Config::NOTION_API_KEY, "User-Agent" => "Crest/0.27.0 (Crystal/1.0.0)"})
-    .to_return(body: {"results": [] of String}.to_json)
+  WebMock.stub(:post, "https://api.notion.com/v1/search").with(
+    body: "{\"query\":\"dummy text\",\"page_size\":10,\"sort\":{\"direction\":\"ascending\",\"timestamp\":\"last_edited_time\"}}",
+    headers: {"Content-Type" => "application/json", "Notion-Version" => "2021-05-13", "Authorization" => "", "User-Agent" => "Crest/0.27.0 (Crystal/1.0.0)"}
+  ).to_return(
+    body: "{\"object\":\"list\",\"results\":[],\"next_cursor\":\"null\",\"has_more\":false}"
+  )
 end
 
 def web_mock_notion_pages
-  WebMock.stub(:post, "https://api.notion.com/v1/pages")
-    .with(body: "{\"parent\":{\"type\":\"page_id\"},\"properties\":{\"title\":[{\"type\":\"text\",\"text\":{\"content\":\"dummy text\"}}]}}", headers: {"Content-Type" => "application/json", "Notion-Version" => AskNotion::Config::NOTION_API_VERSION, "Authorization" => AskNotion::Config::NOTION_API_KEY, "User-Agent" => "Crest/0.27.0 (Crystal/1.0.0)"})
-    .to_return(body: {"results": [] of String, "id": "dummy_id"}.to_json)
+  WebMock.stub(:post, "https://api.notion.com/v1/pages").with(
+    body: "{\"parent\":{\"type\":\"page_id\",\"page_id\":\"639e6e8d-73ab-45c8-a1b0-bf829d17c5e4\"},\"properties\":{\"title\":[{\"type\":\"text\",\"text\":{\"content\":\"dummy text\"}}]}}",
+    headers: {"Content-Type" => "application/json", "Notion-Version" => "2021-05-13", "Authorization" => "", "User-Agent" => "Crest/0.27.0 (Crystal/1.0.0)"}
+  ).to_return(
+    body: "{\"object\":\"page\",\"id\":\"35cebbee-3ab2-4e50-812f-e558d5fdc273\",\"created_time\":\"2021-06-24T15:10:38.213Z\",\"last_edited_time\":\"2021-06-24T15:10:38.213Z\",\"parent\":{\"type\":\"page_id\",\"page_id\":\"639e6e8d-73ab-45c8-a1b0-bf829d17c5e4\"},\"archived\":false,\"properties\":{\"title\":{\"id\":\"title\",\"type\":\"title\",\"title\":[{\"type\":\"text\",\"text\":{\"content\":\"dummy text\",\"link\":\"null\"},\"annotations\":{\"bold\":false,\"italic\":false,\"strikethrough\":false,\"underline\":false,\"code\":false,\"color\":\"default\"},\"plain_text\":\"dummy text\",\"href\":\"null\"}]}}}"
+  )
+end
+
+def web_mock_rocket_message
+  WebMock.stub(:post, "https://osp.rocket.chat/api/v1/chat.sendMessage").with(
+    body: "{\"message\":{\"msg\":\"Bonjour @here, une question a besoin de votre réponse. Si l'un-e d'entre vous a la réponse, n'hésitez pas à la compléter.\",\"rid\":\"AzertYuiop4\",\"tmid\":\"dummy_message_id_1\",\"alias\":\"AskNotion\",\"avatar\":\"https://upload.wikimedia.org/wikipedia/commons/4/45/Notion_app_logo.png\",\"attachments\":[{\"title\":\"dummy text\",\"title_link\":\"/35cebbee3ab24e50812fe558d5fdc273\",\"collapsed\":false}]}}",
+    headers: {"Content-Type" => "application/json", "X-Auth-Token" => "", "X-User-Id" => "", "User-Agent" => "Crest/0.27.0 (Crystal/1.0.0)"}
+  ).to_return(
+    body: "{\"results\": [\"dummy\"], \"id\": \"dummy_id\"}"
+  )
 end
